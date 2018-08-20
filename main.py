78ad1564a069495efb5bad0cb8eed5fbb707d813
agodoriru <takumijohn0806@gmail.com>
2018-08-20 18:07:15 +0900

    [add]sample

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import Completer, Completion, WordCompleter
 import click
 import datetime
+from fuzzyfinder import fuzzyfinder
 
 CommandCompleter = WordCompleter([
-    'add', 'any', 'run', 'fix','show', 'delete', 'config', 'exit', 'ip', 'port', 'dst', 'src'],
+    'add', 'any', 'edit', 'run', 'fix','show', 'delete', 'config', 'exit', 'ip', 'port', 'dst', 'src', 'history'],
     ignore_case = True)
 
 def main():
     now = datetime.datetime.now()
-    print('\n\nacticated     time' + str(now))
+    print('\nacticated     time' + str(now))
 
     while 1:
         try:
@@ -21,20 +22,21 @@ def main():
                                 completer = CommandCompleter,
                                 )
 
-            #print(user_input)
+            print('input:' + user_input)
+            print(type(user_input))
             #click.echo_via_pager(user_input)
             #message = click.edit()    
             # print(user_input)
             #click.edit(filepath) -> edit launch
             if(user_input == 'exit'):
                 break
-
+        
         except KeyboardInterrupt:
             continue
         except EOFError:
             break
 
-    print('good bye')
+    print('Good Bye')
     
 if __name__ == '__main__':
     main()
